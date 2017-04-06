d847493cab26fd1cf7bf823ba05439423568696a
kamontat <kamontat_c@hotmail.com>
2017-04-07 00:17:21 +0700

    update tester

--- data_testing.php
+++ data_testing.php
@@ -9,10 +9,18 @@ function goodbye()
     echo 'good bye';
 }
 
+function not_found()
+{
+    echo 'Not found this. <br>';
+    echo 'Only `say=hello` or `say=bye` accepted';
+}
+
 if ($_GET['say'] == "hello") {
     hello();
-}
-if ($_GET['say'] == "bye") {
-    goodbye();
-}
+} else
+    if ($_GET['say'] == "bye") {
+        goodbye();
+    } else {
+
+    }
 ?>
