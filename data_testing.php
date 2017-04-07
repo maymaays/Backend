dbfa577c4cc263d57bde84509517821c75471057
kamontat <kamontat_c@hotmail.com>
2017-04-08 01:39:16 +0700

    Fixed error and query

--- data_testing.php
+++ data_testing.php
@@ -1,4 +1,6 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+
 function hello()
 {
     echo 'hello world';
@@ -11,15 +13,17 @@ function goodbye()
 
 function not_found()
 {
-    echo 'Not found this. <br>';
-    echo 'Only `?say=hello` or `?say=bye` accepted';
+    echo 'Not found this. Only `?say=hello` or `?say=bye` accepted';
 }
 
 if ($_GET['say'] == "hello") {
     hello();
 } else if ($_GET['say'] == "bye") {
     goodbye();
-} else {
+} else if ($_GET['say']) {
     not_found();
+    http_response_code(400);
+} else {
+    http_response_code(404);
 }
 ?>
