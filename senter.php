97b76fe72c9414ced76341a0ebbbc5657883ea36
kamontat <kamontat_c@hotmail.com>
2017-04-09 16:03:35 +0700

    clean code and delete unuse code

--- senter.php
+++ senter.php
@@ -9,17 +9,15 @@ include 'Model.php';
 include 'api.php';
 include 'json_formating.php';
 
-$select = "SELECT * FROM test WHERE id=0";
-$insert = "INSERT INTO test (id, name, surname) VALUES (0, 'new', 'sur')";
-$delete = "DELETE FROM `test` WHERE name='new'";
-$update = "UPDATE test SET surname='new_sur' WHERE name='new'";
-
+// $select = "SELECT * FROM test WHERE id=0";
+// $insert = "INSERT INTO test (id, name, surname) VALUES (0, 'new', 'sur')";
+// $delete = "DELETE FROM `test` WHERE name='new'";
+// $update = "UPDATE test SET surname='new_sur' WHERE name='new'";
 
 if (isset($_GET['query'])) {
     $query = $_GET['query'];
     $model = new DatabaseModel(false);
     $result = $model->query($query);
     echo "<pre>" . sqlToJSON($result) . "</pre>";
-
     $model->close();
 }
