dba688d143be20218496f22d164869dd6ffff7b4
kamontat <kamontat_c@hotmail.com>
2017-04-09 16:02:59 +0700

    add comment

--- Model.php
+++ Model.php
@@ -40,6 +40,15 @@ class DatabaseModel
         if (!isset($this->database)) die("Not database created.");
     }
 
+    /**
+     *
+     *
+     * @param string $q
+     * @return mysqli_result|boolean For successful SELECT, SHOW, DESCRIBE or
+     * EXPLAIN queries <b>mysqli_query</b> will return
+     * a <b>mysqli_result</b> object.For other successful queries <b>mysqli_query</b> will
+     * return true and false on failure.
+     */
     public function query(string $q)
     {
         // first test, if not exist connect again
