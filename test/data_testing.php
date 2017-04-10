133bb100e9032be5f3db6ac019ac6ecfd9d2d0f4
kamontat <kamontat_c@hotmail.com>
2017-04-10 15:50:24 +0700

    update tester

--- test/data_testing.php
+++ test/data_testing.php
@@ -16,6 +16,8 @@ function not_found()
     echo 'Not found this. Only `hello` or `bye` accepted';
 }
 
+echo $_SERVER['SERVER_ADDR'];
+
 if (
     isset($_SERVER['REMOTE_ADDR']) AND ($_SERVER['REMOTE_ADDR'] !== $_SERVER['SERVER_ADDR'])
 ) {
