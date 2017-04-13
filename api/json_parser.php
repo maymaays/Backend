04628a8e5da70073628b91cc5a93f1dffa833ad0
kamontat <kamontat_c@hotmail.com>
2017-04-13 21:05:51 +0700

    re json parser, using php api

--- api/json_parser.php
+++ api/json_parser.php
@@ -6,24 +6,20 @@
  * Time: 10:08 PM
  */
 
-$default_tag = "<pre style=\"word-wrap: break-word; white-space: pre-wrap;\">";
-$default_tag_close = "</pre>";
-
+/**
+ * @param bool $success
+ * @param array|null $array
+ * @return string json object (UTF-8 code ONLY)
+ */
 function toJSON(bool $success, array $array = null /* mapping array */)
 {
-    // $array will be like: `[ id: [1, 4, 5], name: ["first", "second"] ]` (Map<String, Object[]> java syntax)
-    // real
-    $string = sprintf("{\n\t\"success\": \"%s\"", $success ? "true" : "false");
+    $array_success = array('success' => $success ? "true" : "false");
     if (isset($array)) {
-        foreach ($array as $key => $values) {
-            $string .= ",\n\t\"" . $key . "\"" . ": [";
-            foreach ($values as $value) {
-                $string .= "\"" . $value . "\",";
-            }
-            $string = substr($string, 0, -1) . "]";
-        }
+        $result = array_merge($array_success, $array);
+        return json_encode($result, JSON_PRETTY_PRINT);
+    } else {
+        return json_encode($array_success, JSON_PRETTY_PRINT);
     }
-    return $string . "\n}";
 }
 
 /**
@@ -42,14 +38,3 @@ function sqlToJSON($result)
 }
 
 ?>
-
-
-<html>
-<style type="text/css">
-    pre {
-        -moz-tab-size: 2; /* Firefox 4+ */
-        -o-tab-size: 2; /* Opera 11.5 & 12.1 only */
-        tab-size: 2; /* Chrome 21+, Safari 6.1+, Opera 15+ */
-    }
-</style>
-</html>
