2c3e79911bc43f174186bc0bc1175670f3efc4e4
kamontat <kamontat_c@hotmail.com>
2017-04-14 14:00:57 +0700

    update select_all

--- Getting.php
+++ Getting.php
@@ -1,9 +1,45 @@
 <?php
 
 include $_SERVER['DOCUMENT_ROOT'] . '/api/query_api.php';
+include $_SERVER['DOCUMENT_ROOT'] . '/helper/Information.php';
 
 $action = $_GET['action'];
 
-echo selectAll("test", array("id=1", "name='net'"));
+$array = Information::get_required_parameter($action);
+if (!isset($array)) {
+    http_response_code(501);
+    die(failureToJSON($action . " isn't implementation yet!"));
+}
+
+$str = "";
+$error = false;
+foreach ($array as $required) {
+    if (!isset($_GET[$required])) {
+        $error = true;
+        $str .= "\"" . $required . "\"" . " ";
+    }
+}
+if ($error) {
+    http_response_code(400);
+    die(failureToJSON($str . " is required for " . $action . " action"));
+}
+
+switch ($action) {
+    case "select_all":
+        selectAll($_GET[$array[0]], $_GET[$array[1]]);
+        break;
+    case "insert_customer":
+
+        break;
+    case "update_customer":
+
+        break;
+    case "search_customer":
+
+        break;
+    default:
+
+        break;
+}
 
 ?>
