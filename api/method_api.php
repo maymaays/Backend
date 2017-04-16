2647be580b8091e69af722e4894bcec01a0052a1
kamontat <kamontat_c@hotmail.com>
2017-04-16 12:17:01 +0700

    add more useful method 1. debug in console 2. convert array to string

--- api/method_api.php
+++ api/method_api.php
@@ -42,3 +42,35 @@ function to_array(mysqli_result $result)
     }
     return $array;
 }
+
+
+/**
+ * @param array $arr not null
+ * @param string $between
+ * @param string|null $first
+ * @param string|null $last
+ * @return string converted array
+ */
+function convert_array(array $arr, $between, $first = null, $last = null)
+{
+    $str = null;
+    foreach ($arr as $item) {
+        if (!isset($str)) {
+            $str = isset($first) ? $first . $item : $item;
+        } else {
+            $str .= $between . $item;
+        }
+    }
+    if (isset($last) and isset($str))
+        $str .= $last;
+    return $str;
+}
+
+function debug_to_console($data)
+{
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+}
